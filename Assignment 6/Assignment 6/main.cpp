1ff9bc53a8e4febfa163aff04482ce232bf33ba7
Alvin Teng <alvinteng@Alvins-MBP.attlocal.net>
2017-09-18 23:33:27 -0700

    Assignment 6

--- Assignment 6/Assignment 6/main.cpp
+++ Assignment 6/Assignment 6/main.cpp
@@ -8,10 +8,30 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
+int main() {
     // insert code here...
-    int userInt, remainder;
-    char quit = 'q';
+    using namespace std;
+    int input = -1;
+    char quit = 'a';
+    
+    while(quit != 'q')
+    {
+        while(input < 0)
+        {
+            cout << "Type a number: ";
+            cin >> input;
+        }
+        
+        if (input%2 == 0)
+        {
+            cout << "Your number is EVEN.\n";
+        } else {
+            cout << "Your number is ODD.\n";
+        }
+        cout << "Type anything for another number or type 'q' to quit: ";
+        cin >> quit;
+    }
+    
     
     return 0;
 }
